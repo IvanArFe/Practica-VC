37a2c2dc83ea1ec1b39d1e1873146f6f70052834
Ivan Arenal <ivan.arenal@estudiants.urv.cat>
2025-05-23 12:08:52 +0200

    Base funcional. Binarització correcta i extreta informació només de cucs.

--- IdentificacioCucs.m
+++ IdentificacioCucs.m
@@ -24,38 +24,26 @@ for i=1:1
 
     % Creem una màscara per eliminar la zona blanca de la placa
     mascPlaca = imgNorm > 0.1; % píxels més brillants que 10%
-    mascPlaca = imfill(mascPlaca, "holes");
-    mascPlaca = imopen(mascPlaca, strel('disk', 10));
 
-    % Màscara circular per eliminar zona concreta
-    [H, W] = size(imgFiltrada);
-    [X, Y] = meshgrid(1:W, 1:H);
-    xc = W/2; yc = H/2;
-    radi = min(H, W) * 0.45;  % Ajustable segons imatge
-    zonaClara = ((X - xc).^2 + (Y - yc).^2) < radi^2;
-
-    figure;
-    imshow(zonaClara);
-    title("Zona a eliminar (màscara circular)");
-
-    % Apliquem màscara a imatge filtrada
-    imgMasc = imgFiltrada;
-    imgMasc(~mascPlaca) = 0; % Fons negre
+    %figure;
+    %imshow(~mascPlaca);
+    %title('mascara a aplicar');
     
     % Apliquem binarització adaptativa amb threshold perquè tenim
     % il·luminació desigual
-    th = adaptthresh(imgMasc, 0.3);
-    imgBinaria = imbinarize(imgMasc, th);
-    imgBinaria = ~imgBinaria;
-
+    th = adaptthresh(imgNorm, 0.5);
+    imgBinaria = imbinarize(imgNorm, th);
+    imgBinaria = ~imgBinaria; % Cucs en blanc, fons en negre.
+        
     % Aplicar màscares per eliminar soroll
-    imgBinaria(~mascPlaca) = 0;
-    % Definim zona massa clara dins la màscara circular
-    zonaRealmentClara = zonaClara & imgNorm > 0.70;  % només el blanc pur
+    mascErosio = imerode(mascPlaca, strel('disk', 4)); % Eliminem vorera de la màscara
+    imgBinaria(~mascErosio) = 0;
 
-    % Eliminem només aquesta zona clara
-    imgBinaria(zonaRealmentClara) = 0;
-
+    % Detectem únicament cucs per eliminar la zona blanca.
+    cucs = ~imbinarize(imgNorm, 0.45);
+    imgBinaria(~cucs) = 0;
+    
+    % Comparem imatge filtrada i binària per veure els resultats.
     figure;
     subplot(1,2,1);
     imshow(imgFiltrada);
